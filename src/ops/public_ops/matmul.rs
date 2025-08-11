5c7d11afa290fee7f00eb42844a47b6dfd4eb869
Hy-LeTuan <letuanhy1507@gmail.com>
2025-08-11 15:02:52 -0500

    add matmul public API

---
+++ src/ops/public_ops/matmul.rs
@@ -0,0 +1,14 @@
+use ndarray::LinalgScalar;
+use std::fmt::Debug;
+use std::ops::Add;
+
+use crate::ops::public_ops::matmul_public::matmul_tensor_tensor;
+use crate::tensor_core::dtypes::DTComp;
+use crate::tensor_core::tensor::Tensor;
+
+pub fn matmul<T>(lhs_tensor: &Tensor<T>, rhs_tensor: &Tensor<T>) -> Tensor<T>
+where
+    T: DTComp + Clone + Debug + Add<Output = T> + 'static + LinalgScalar,
+{
+    return matmul_tensor_tensor(lhs_tensor, rhs_tensor);
+}
