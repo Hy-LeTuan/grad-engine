7de86cd324c96e7f11b72ef73b195f5172568551
Hy-LeTuan <letuanhy1507@gmail.com>
2025-08-15 12:39:48 -0500

    update build backward graph to create full animation of backward graph, minor refractor

--- animation/graph/graph_anim_structures.py
+++ animation/graph/graph_anim_structures.py
@@ -1,6 +1,9 @@
-from graph.graph_structures import ForwardNode, BackwardNode
+from graph.graph_raw_representations import ForwardNode, BackwardNode
+from graph.graph_control_structure import NodeLayer, TensorLayer, EdgeBetweenLayerMembers
+
 from manim import *
 import numpy as np
+import re
 
 
 def create_anim_computation_node(node: BackwardNode | ForwardNode, forward=False):
@@ -8,15 +11,24 @@ def create_anim_computation_node(node: BackwardNode | ForwardNode, forward=False
 
 
 def create_anim_node_from_backward_node(node: BackwardNode) -> VGroup:
-    node_text = Text(node.get_name(), color=WHITE).scale(0.5)
+    if node.get_name() == "GradAccum":
+        node_text = Text("Accum", color=WHITE).scale(0.8)
+    else:
+        split_string = re.findall('[A-Z][a-z]*', node.get_name())
+
+        start_text = Text(split_string[0], color=WHITE).scale(0.8)
+
+        end_text = Text(split_string[1], color=WHITE).scale(0.8)
+
+        node_text = VGroup(start_text, end_text).arrange(DOWN, buff=0.2)
 
     node_circle = Circle(color=BLUE, stroke_width=4, stroke_color=BLUE,
                          fill_color=BLUE, fill_opacity=0.8, radius=1.8)
 
     node_text.move_to(node_circle.get_center())
-    node = VGroup(node_circle, node_text)
+    new_node = VGroup(node_circle, node_text)
 
-    return node
+    return new_node
 
 
 def create_anim_node_from_forward_node(node: ForwardNode) -> VGroup:
@@ -31,14 +43,14 @@ def create_anim_node_from_forward_node(node: ForwardNode) -> VGroup:
 
 def create_origin_anim_tensor_from_backward_node(node: BackwardNode) -> MobjectMatrix:
     origin: np.ndarray = node.get_origin().get_data()
-    shape = Text("Shape=(" + ", ".join([str(d) for d in origin.shape]) + ")")
+    shape = Text("(" + ", ".join([str(d) for d in origin.shape]) + ")")
 
     if origin.ndim == 0:
-        name = Text("Sca. Tensor")
+        name = Text("Sca")
     elif origin.ndim == 1:
-        name = Text("Vec. Tensor")
+        name = Text("Vec")
     else:
-        name = Text("Mat. Tensor")
+        name = Text("Mat")
 
     cell_content = VGroup(name, shape).arrange(
         DOWN, buff=0.5)
@@ -57,3 +69,62 @@ def create_arrow_to_connect_node(start_node, end_node, color=WHITE, stroke_width
     ), color=color, stroke_width=stroke_width, tip_length=tip_length, buff=buff)
 
     return arrow
+
+
+def build_anim_backward_graph(starting_nodes, parent_node_layer, layer_list: list):
+    tensor_layer = TensorLayer(
+        convert=create_origin_anim_tensor_from_backward_node)
+
+    if tensor_layer not in layer_list:
+        layer_list.append(tensor_layer)
+
+    if parent_node_layer not in layer_list:
+        layer_list.append(parent_node_layer)
+
+    next_node_layer = NodeLayer(
+        convert=create_anim_computation_node, forward=False)
+
+    next_starting_nodes = []
+
+    for node in starting_nodes:
+        (parent_anim_node_id, parent_anim_node) = parent_node_layer.safe_append_and_return(
+            node, with_id=True)
+
+        (tensor_anim_id, tensor_anim) = tensor_layer.safe_append_and_return(
+            node, with_id=True)
+
+        parent_node_layer.append_edge(EdgeBetweenLayerMembers(
+            tensor_layer, tensor_anim_id, parent_node_layer, parent_anim_node_id))
+
+        if len(node.get_children()) > 0:
+
+            for child in node.get_children():
+                (child_anim_node_id, child_anim_node) = next_node_layer.safe_append_and_return(
+                    child, with_id=True)
+
+                tensor_layer.append_edge(EdgeBetweenLayerMembers(
+                    tensor_layer, tensor_anim_id, next_node_layer, child_anim_node_id))
+
+                next_starting_nodes.append(child)
+
+    if len(next_starting_nodes) > 0:
+        build_anim_backward_graph(
+            next_starting_nodes, next_node_layer, layer_list)
+
+
+def position_layer_list_horizontal(layer_list: list[NodeLayer | TensorLayer], total_width: float, spacing: float):
+    max_obj_width = 0
+
+    max_obj_width = 0
+
+    for layer in layer_list:
+        if not layer.get_is_setup():
+            layer.setup()
+        max_obj_width = max(max_obj_width, layer.get_display_group().width)
+
+    slot_width = spacing
+
+    if max_obj_width > slot_width:
+        scale_factor = slot_width / max_obj_width
+        for layer in layer_list:
+            layer.get_display_group().scale(scale_factor)
--- animation/main.py
+++ animation/main.py
@@ -1,8 +1,42 @@
 from manim import *
-from graph.graph_structures import Graph
+from graph.graph_raw_representations import Graph
 from graph.parse_utils import parse_graph_from_json
-from graph.graph_anim_structures import create_anim_computation_node, create_arrow_to_connect_node, create_origin_anim_tensor_from_forward_node
+from graph.graph_anim_structures import create_anim_computation_node, create_arrow_to_connect_node, create_origin_anim_tensor_from_forward_node, create_origin_anim_tensor_from_backward_node, build_anim_backward_graph, position_layer_list_horizontal
 from graph.graph_control_structure import NodeLayer, TensorLayer, EdgeBetweenLayerMembers
+
+
+def build_anim_forward_graph(starting_nodes, parent_node_layer, layer_list: list):
+    tensor_layer = TensorLayer(
+        convert=create_origin_anim_tensor_from_forward_node)
+
+    layer_list.append(parent_node_layer)
+    layer_list.append(tensor_layer)
+
+    for node in starting_nodes:
+        (parent_anim_node_id, parent_anim_node) = parent_node_layer.safe_append_and_return(
+            node, with_id=True)
+
+        (tensor_anim_id, tensor_anim) = tensor_layer.safe_append_and_return(
+            node, with_id=True)
+
+        parent_node_layer.append_edge(EdgeBetweenLayerMembers(
+            parent_node_layer, parent_anim_node_id, tensor_layer, tensor_anim_id))
+
+        # parent node -> tensor (result of that node) -> compute function that the tensor contributes to
+
+        if len(node.get_children()) > 0:
+            next_node_layer = NodeLayer(
+                convert=create_anim_computation_node, forward=True)
+
+            for child in node.get_children():
+                (child_anim_node_id, child_anim_node) = next_node_layer.safe_append_and_return(
+                    child, with_id=True)
+
+                tensor_layer.append_edge(EdgeBetweenLayerMembers(
+                    tensor_layer, tensor_anim_id, next_node_layer, child_anim_node_id))
+
+            build_anim_forward_graph(
+                node.get_children(), next_node_layer, layer_list)
 
 
 class CreateGraph(Scene):
@@ -12,83 +46,60 @@ class CreateGraph(Scene):
     def get_graph(self) -> Graph:
         return self.graph
 
-    def build_forward_graph(self, starting_nodes, parent_node_layer, layer_list: list):
-        tensor_layer = TensorLayer(
-            convert=create_origin_anim_tensor_from_forward_node)
-
-        layer_list.append(parent_node_layer)
-        layer_list.append(tensor_layer)
-
-        for node in starting_nodes:
-            (parent_anim_node_id, parent_anim_node) = parent_node_layer.safe_append_and_return(
-                node, with_id=True)
-
-            (tensor_anim_id, tensor_anim) = tensor_layer.safe_append_and_return(
-                node, with_id=True)
-
-            parent_node_layer.append_edge(EdgeBetweenLayerMembers(
-                parent_node_layer, parent_anim_node_id, tensor_layer, tensor_anim_id))
-
-            # parent node -> tensor (result of that node) -> compute function that the tensor contributes to
-
-            if len(node.get_children()) > 0:
-                next_node_layer = NodeLayer(
-                    convert=create_anim_computation_node, forward=True)
-
-                for child in node.get_children():
-                    (child_anim_node_id, child_anim_node) = next_node_layer.safe_append_and_return(
-                        child, with_id=True)
-
-                    tensor_layer.append_edge(EdgeBetweenLayerMembers(
-                        tensor_layer, tensor_anim_id, next_node_layer, child_anim_node_id))
-
-                self.build_forward_graph(
-                    node.get_children(), next_node_layer, layer_list)
-
     def animate_forward_graph(self):
         starting_nodes = self.graph.get_forward_nodes()
-        layer_list = []
-        parent_node_layer = NodeLayer(convert=create_anim_computation_node)
+        layer_list: list[NodeLayer] = []
+        parent_node_layer = NodeLayer(
+            convert=create_anim_computation_node, forward=True)
 
-        self.build_forward_graph(
+        build_anim_forward_graph(
             starting_nodes, parent_node_layer, layer_list)
 
-        for (i, edge) in enumerate(parent_node_layer.get_edges()):
-            origin = edge.get_origin()
-            destination = edge.get_destination()
+        total_width = 1.0 * config.frame_width  # 90% of screen width
+        spacing = (total_width / (len(layer_list) - 1)) * 1.05
 
-            origin_node = origin.get_conneted_mem()
-            destination_node = destination.get_conneted_mem()
-
-            self.play(FadeIn(origin_node))
-            self.play(FadeIn(destination_node))
-
+        max_obj_width = 0
         for layer in layer_list:
-            print(layer)
+            if not layer.get_is_setup():
+                layer.setup()
+            max_obj_width = max(max_obj_width, layer.get_display_group().width)
+
+        slot_width = spacing
+
+        if max_obj_width > slot_width:
+            scale_factor = slot_width / max_obj_width
+            for layer in layer_list:
+                layer.get_display_group().scale(scale_factor)
+
+        for i, layer in enumerate(layer_list):
+
+            layer.get_display_group().move_to(
+                LEFT * (total_width / 2) + RIGHT * (i * spacing)
+            )
+
+            layer.display(self)
 
     def animate_backward_graph(self):
-        root = self.graph.get_root()
+        root = [self.get_graph().get_root()]
+        layer_list: list[NodeLayer | TensorLayer] = []
+        parent_node_layer = NodeLayer(
+            convert=create_anim_computation_node, forward=False)
 
-        root_node = create_anim_computation_node(root, forward=False)
-        self.play(FadeIn(root_node), root_node.animate.shift(LEFT * 5))
+        # create layers and populate layer_list
+        build_anim_backward_graph(
+            root, parent_node_layer, layer_list)
 
-        anchor = root_node
+        # position all layers
+        total_width = 0.9 * config.frame_width
+        spacing = total_width / (len(layer_list) - 1)
+        position_layer_list_horizontal(
+            layer_list=layer_list, total_width=total_width, spacing=spacing)
 
-        for i, child in enumerate(root.get_children()):
-            child_anim_node = create_anim_computation_node(
-                child, forward=False)
-            child_anim_node.next_to(
-                anchor, direction=RIGHT, buff=(0.4))
-
-            self.play(FadeIn(child_anim_node), )
-            self.add(child_anim_node)
-
-            arrow = create_arrow_to_connect_node(root_node, child_anim_node)
-
-            self.play(FadeIn(arrow))
-            self.add(arrow)
-
-            anchor = child_anim_node
+        for i, layer in enumerate(layer_list):
+            layer.get_display_group().move_to(
+                LEFT * (total_width / 2) + RIGHT * (i * spacing)
+            )
+            layer.display(self)
 
     def construct(self):
-        self.animate_forward_graph()
+        self.animate_backward_graph()
